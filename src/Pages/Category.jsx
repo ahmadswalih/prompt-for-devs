166aa77473f0ea0eb88f204eaf2c49fac6bb583d
ahmadswalih <ahmadswalih777@gmail.com>
2024-03-11 23:24:11 +0530

    added pagination

--- src/Pages/Category.jsx
+++ src/Pages/Category.jsx
@@ -4,7 +4,9 @@ import { Prompts } from "../components";
 
 const CategoryPage = () => {
   const { categoryName } = useParams();
-  const [prompt, setPrompts] = useState([]);
+  const [prompts, setPrompts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const promptsPerPage = 6;
 
   useEffect(() => {
     fetch(`/data/${categoryName}.json`)
@@ -14,6 +16,12 @@ const CategoryPage = () => {
         console.error("Error fetching or parsing data:", error)
       );
   }, [categoryName]);
+
+  const indexOfLastPrompt = currentPage * promptsPerPage;
+  const indexOfFirstPrompt = indexOfLastPrompt - promptsPerPage;
+  const currentPrompts = prompts.slice(indexOfFirstPrompt, indexOfLastPrompt);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div className="text-black items-center   flex mx-auto my-auto flex-col justify-center mt-16">
@@ -26,12 +34,24 @@ const CategoryPage = () => {
         Popular Prompts for : <br />{" "}
         <span className="font-bold">{categoryName}</span>
       </p>
-      {console.log(prompt)}
       <div className="mt-10">
-        {prompt.map((value) => {
-          // console.log(value);
-          return <Prompts key={value.id} prompt={value.value} />;
-        })}
+        {currentPrompts.map((value) => (
+          <Prompts key={value.id} prompt={value.value} />
+        ))}
+      </div>
+      <div className="flex justify-center mt-4">
+        {Array.from(
+          { length: Math.ceil(prompts.length / promptsPerPage) },
+          (_, i) => (
+            <button
+              key={i}
+              onClick={() => paginate(i + 1)}
+              className="mx-1 px-3 py-1 mb-10 bg-gray-300 hover:bg-gray-400 focus:outline-none rounded"
+            >
+              {i + 1}
+            </button>
+          )
+        )}
       </div>
     </div>
   );
